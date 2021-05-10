30debf0b187b64ee9b0b4ebb2accb5584edac414
JJJ4032002 <rp4888888@gmail.com>
2021-05-10 19:14:34 +0530

    good commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const reviews = [{
+    img : "F:\Github Projects\reviews\Images\pexels-monstera-6311541.jpg"
+    ,
+    h3 : "Marin"
+    ,
+    p : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt et hic fuga odio exercitationem sunt obcaecati maxime optio, tenetur iusto blanditiis cum inventore praesentium suscipit? Non deserunt sit officia hic."
+},{
+    img : "F:\Github Projects\reviews\Images\pexels-monstera-6311505.jpg",
+    h3 : "John"
+    ,
+    p : "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed quaerat soluta nam hic non animi blanditiis eaque quis, atque pariatur in. Exercitationem tempora nesciunt odit, sed ducimus ipsum nostrum? Veritatis."},{
+        img : "F:\Github Projects\reviews\Images\pexels-monstera-6311547.jpg"
+        ,
+        h3 : "Joanna"
+    }]
